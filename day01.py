bbb26faa0fdb8c6450ff2e28067ab757584872ab
Sebastian Ross <sebastian.ross@svt.se>
2021-12-01 23:32:46 +0100

    Make it smaller

--- day01.py
+++ day01.py
@@ -1,15 +1,11 @@
 with open("inputs/day1.txt") as file:
     data = [int(line) for line in file]
 
-# 1
-part1 = 0
-for i, val in enumerate(data):
-    if i > 0 and val > data[i - 1]:
-        part1 += 1
-
-# 2
+part1 = 0 
 part2 = 0
-for i in range(len(data)):
+for i, val in enumerate(data):
+    if val > data[i - 1]:
+        part1 += 1
     if i > 0 and sum(data[i:i+3]) > sum(data[i-1:i+2]):
         part2 += 1
 
