c5a0420567bfcfcce3fa21f762f941aadc668034
Sebastian Ross <sebastian.ross@svt.se>
2021-12-02 10:48:30 +0100

    list comprehension is cool

--- day02.py
+++ day02.py
@@ -2,34 +2,31 @@ with open("inputs/day2.txt") as file:
     data = [line for line in file]
 
 # Part 1
-position_matrix = [0, 0] #[horisontal, depth]
-part1 = 0
+position_matrix = [0, 0]  # [horisontal, depth]
 for entry in data:
-    [cmd, nr] = entry.split()
+    [cmd, nr] = [int(x) if x.isdigit() else x for x in entry.split()]
 
     if cmd == 'down':
-        position_matrix[1] += int(nr)
+        position_matrix[1] += nr
     elif cmd == 'up':
-        position_matrix[1] -= int(nr)
+        position_matrix[1] -= nr
     else:
-        position_matrix[0] += int(nr)
+        position_matrix[0] += nr
 
 part1 = position_matrix[0] * position_matrix[1]
 
 # Part 2
-position_matrix = [0, 0, 0] #[horisontal, depth, aim]
-part2 = 0
-
+position_matrix = [0, 0, 0]  # [horisontal, depth, aim]
 for entry in data:
-    [cmd, nr] = entry.split()
+    [cmd, nr] = [int(x) if x.isdigit() else x for x in entry.split()]
 
     if cmd == 'down':
-        position_matrix[2] += int(nr)
+        position_matrix[2] += nr
     elif cmd == 'up':
-        position_matrix[2] -= int(nr)
+        position_matrix[2] -= nr
     else:
-        position_matrix[0] += int(nr)
-        position_matrix[1] += position_matrix[2] * int(nr)
+        position_matrix[0] += nr
+        position_matrix[1] += position_matrix[2] * nr
 
 part2 = position_matrix[0] * position_matrix[1]
 
