6cef1de4478cf306b28aa79dabe00d62f85464a5
Sebastian Ross <sebastian.ross@svt.se>
2021-12-02 11:39:56 +0100

    more day 2 fixes

--- day02.py
+++ day02.py
@@ -1,11 +1,10 @@
 with open("inputs/day2.txt") as file:
-    data = [line for line in file]
+    data = [x for x in [line.split() for line in file]]
 
 # Part 1
 position_matrix = [0, 0]  # [horisontal, depth]
-for entry in data:
-    [cmd, nr] = [int(x) if x.isdigit() else x for x in entry.split()]
-
+for [cmd, val] in data:
+    nr = int(val)
     if cmd == 'down':
         position_matrix[1] += nr
     elif cmd == 'up':
@@ -16,17 +15,17 @@ for entry in data:
 part1 = position_matrix[0] * position_matrix[1]
 
 # Part 2
-position_matrix = [0, 0, 0]  # [horisontal, depth, aim]
-for entry in data:
-    [cmd, nr] = [int(x) if x.isdigit() else x for x in entry.split()]
-
+position_matrix = [0, 0]  # [horisontal, depth]
+aim = 0
+for [cmd, val] in data:
+    nr = int(val)
     if cmd == 'down':
-        position_matrix[2] += nr
+        aim += nr
     elif cmd == 'up':
-        position_matrix[2] -= nr
+        aim -= nr
     else:
         position_matrix[0] += nr
-        position_matrix[1] += position_matrix[2] * nr
+        position_matrix[1] += aim * nr
 
 part2 = position_matrix[0] * position_matrix[1]
 
