30c15b4e0bd491272746836b0d4b78a0affe1dd2
Sebastian Ross <sebastian.ross@svt.se>
2021-12-05 12:43:32 +0100

    Fixes

--- day05.py
+++ day05.py
@@ -2,6 +2,7 @@ with open("inputs/day5.txt") as file:
     data = [[int(item) for sublist in [x[0].split(','), x[1].split(',')]
              for item in sublist] for x in [line.strip().split(' -> ') for line in file]]
 
+# Part 1
 nr_dic = {}
 part1 = 0
 for line in data:
@@ -22,6 +23,7 @@ for line in data:
         if nr_dic[key] == 2:
             part1 += 1
 
+# Part 2
 part2 = part1
 for line in data:
     sorted_x = sorted([line[0], line[2]])
@@ -29,18 +31,21 @@ for line in data:
     x_nrs = list(range(sorted_x[0], sorted_x[1] + 1))
     y_nrs = list(range(sorted_y[0], sorted_y[1] + 1))
 
+    if len(x_nrs) != len(y_nrs):
+        continue
+
     if line[1] > line[3]:
         y_nrs.reverse()
     
     if line[0] > line[2]:
         x_nrs.reverse()
 
-    if len(x_nrs) == len(y_nrs):
-        for idx, val in enumerate(x_nrs):
-            key = str(x_nrs[idx]) + ',' + str(y_nrs[idx])
-            nr_dic[key] = nr_dic[key] + 1 if key in nr_dic else 1
-            if nr_dic[key] == 2:
-                part2 += 1
+    for idx, val in enumerate(x_nrs):
+        key = str(x_nrs[idx]) + ',' + str(y_nrs[idx])
+        nr_dic[key] = nr_dic[key] + 1 if key in nr_dic else 1
+        if nr_dic[key] == 2:
+            part2 += 1
 
+# =====================
 print("Part 1:", part1)
 print("Part 2:", part2)
