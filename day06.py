92acf38c6b99d08d25f3ba5b0ed9e34cdaaba6eb
Sebastian Ross <sebastian.ross@svt.se>
2021-12-07 08:56:25 +0100

    day 6 finally

---
+++ day06.py
@@ -0,0 +1,19 @@
+with open("inputs/day6.txt") as file:
+    data = [int(item) for sublist in [line.strip().split(',') for line in file] for item in sublist] # [1,3,2,4,1,3,....]
+
+def get_nr_if_fishes(days):
+    lanterns = [0 for _ in range(9)]
+    for nr in data:
+        lanterns[nr] += 1
+    
+    for _ in range(days):
+        lanterns[7] += lanterns[0]
+        lanterns.append(lanterns.pop(0))
+    
+    return sum(lanterns)
+
+part1 = get_nr_if_fishes(80)
+part2 = get_nr_if_fishes(256)
+
+print("Part 1:", part1)
+print("Part 2:", part2)
